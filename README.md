8a2885009c461440649495d074f829e6c3d4de78
Tyler Eon <kolorahl@gmail.com>
2021-08-23 15:36:35 -0700

    Initial separation of S3 components from Waffle core library
    Needs to be sufficiently tested **and deployed** prior to removing S3
    components from the core Waffle library. Then we should do a major
    version increment of Waffle and have people add `:waffle_s3` to their
    deps where necessary.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Waffle S3 integration
+# Waffle for AWS S3
 
-TODO
+Waffle is a flexible file upload library for Elixir. This is an integration module for Waffle and AWS S3.
+
+[Documentation](https://hexdocs.pm/waffle_s3)
+
+## Usage
+
+An example for setting up `Waffle.Storage.S3` (`config.exs`):
+
+```
+config :waffle,
+  storage: Waffle.Storage.S3,
+  bucket: "custom_bucket",                # or {:system, "AWS_S3_BUCKET"}
+  asset_host: "http://static.example.com" # or {:system, "ASSET_HOST"}
+
+config :ex_aws,
+  json_codec: Jason
+  # any configurations provided by https://github.com/ex-aws/ex_aws
+```
+
+## License
+
+Copyright 2019 Boris Kuznetsov <me@achempion.com>
+
+Copyright 2015 Sean Stavropoulos
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
---
+++ mix.exs
@@ -0,0 +1,74 @@
+defmodule Waffle.Mixfile do
+  use Mix.Project
+
+  @version "1.1.5"
+
+  def project do
+    [
+      app: :waffle_s3,
+      version: @version,
+      elixir: "~> 1.4",
+      deps: deps(),
+      docs: docs(),
+
+      # Hex
+      description: description(),
+      package: package()
+    ]
+  end
+
+  defp description do
+    """
+    Waffle integration for AWS S3.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Boris Kuznetsov"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/elixir-waffle/waffle_s3"},
+      files: ~w(mix.exs README.md CHANGELOG.md lib)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Waffle S3",
+      extras: [
+        "documentation/examples/local.md",
+        "documentation/examples/s3.md"
+      ]
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [
+        :logger,
+        # Used by Mix.generator.embed_template/2
+        :eex
+      ]
+    ]
+  end
+
+  defp deps do
+    [
+      {:waffle, "~> 1.1"},
+
+      # If using Amazon S3
+      {:ex_aws, "~> 2.1"},
+      {:ex_aws_s3, "~> 2.1"},
+      {:sweet_xml, "~> 0.6"},
+
+      # Test
+      {:mock, "~> 0.3", only: :test},
+
+      # Dev
+      {:ex_doc, "~> 0.21", only: :dev},
+
+      # Dev, Test
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
